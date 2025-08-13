f0c60ad9b21d4195ed98b03a9a00461ead249df9
Timofii <tisenkot268@gmail.com>
2025-08-13 19:26:12 +0200

    add
    add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { getImagesByQuery } from "./js/pixabay.js";
+import { getImagesByQuery } from "./js/pixabay-api.js";
 import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions.js";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
